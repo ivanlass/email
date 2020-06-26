8b452846123706204d541188d6cab1086f9dde91
Your Name <you@example.com>
2020-06-26 08:08:41 +0200

    first commit

---
+++ backend/Models/User.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    role: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
+    drzava: {
+        type: String,
+        required: false,
+    },
+    projekat: {
+        type: String,
+        required: false,
+    },
+    poslao: {
+        type: Number,
+        required: false,
+    },
+    mailoviZaSlanje: {
+        type: Array,
+        required: false,
+    },
+    affiID: {
+        type: Array,
+        required: false,
+    },
+    affiID: {
+        type: String,
+        required: false,
+    },
+
+
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+// const fileUpload = require('express-fileupload')
+// var publicDir = require('path').join(__dirname, '/photos/');
+
+// require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// app.use(express.static(publicDir));
+app.use(cors())
+app.use(express.json())
+
+
+// const uri = "mongodb+srv://ivan:ivan@mailreader-pw9x4.mongodb.net/<dbname>?retryWrites=true&w=majority"
+// mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+// const connection = mongoose.connection
+// connection.once('open', () => {
+//     console.log('mongo konekcija ok')
+// })
+
+// const postsRouter = require('./routes/posts')
+// const usersRouter = require('./routes/users')
+// const categoryRouter = require('./routes/categories')
+
+// app.use('/posts', postsRouter)
+// app.use('/users', usersRouter)
+// app.use('/categories', categoryRouter)
+
+app.listen(port, () => {
+    console.log(`port je ${port}`)
+})
